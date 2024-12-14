43ed96c152ba19fce7c8a0d7ab2bebd5231ac29c
Lugard Agu <107192340+lugard1@users.noreply.github.com>
2024-12-14 21:01:05 +0100

    Update

---
+++ index.js
@@ -0,0 +1,27 @@
+const inputEl = document.querySelector('.input');
+
+const bodyEl = document.querySelector('body');
+
+// console.log(inputEl.checked);
+inputEl.checked = localStorage.getItem('mode');
+
+updateBody()
+
+function updateBody(){
+  if(inputEl.checked){
+    bodyEl.style.background = '#000';
+  }else{
+    bodyEl.style.background = '#fff'
+  }
+}
+
+inputEl.addEventListener('input', ()=>{
+  updateBody();
+  updateLocalStorage()
+})
+
+function updateLocalStorage(){
+  localStorage.setItem('mode',
+    JSON.stringify(inputEl.checked)
+  );
+}
